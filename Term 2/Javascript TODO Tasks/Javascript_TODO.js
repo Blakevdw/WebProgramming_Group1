0a6486779ec362cc76ebd05acd6db4bd175168c8
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-05-27 09:34:53 +1000

    Created Cookie Clicker Files

--- Term 2/Javascript TODO Tasks/Javascript_TODO.js
+++ Term 2/Javascript TODO Tasks/Javascript_TODO.js
@@ -1,7 +1,3 @@
-(function($){
-    $('#myform').submit(function(e){
-        var val = $('#in').val();
-        $('ul.list').append('<li>' + val + '</li>');
-        e.preventDefault();
-    });
-})(jQuery);
+function clearAll(){
+    document.getElementById("checkList").innerHTML = "";
+}
