1c45befe55736f79a91f37edc6cbe52fd5f74e2f
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-06-03 09:47:55 +1000

    Multiplier Initiated

--- Term 2/Javascript Cookie Clicker/Cookie_Clicker.js
+++ Term 2/Javascript Cookie Clicker/Cookie_Clicker.js
@@ -1,46 +1,63 @@
 var display = document.getElementById('display');
 var multiply = document.getElementById('multiply');
 var cookie = document.getElementById('cookie');
+var autoclick = document.getElementById('autoclick');
 
-var multiplierCost = 20;
+var multiplierCost = 15;
+var autoclickCost = 50;
+
+var autoclickOn = false;
 
 var score = 0;
 var clickValue = 1;
 var multiplier = 1;
 
 cookie.onclick = increaseScore;
+multiply.onclick = increaseMultiplier
 
 function displayScore() {
     display.innerHTML = score;
 }
 
 function displayMultiplier() {
+    console.log(multiplier);
     multiply.value = 'Multiplier x' + multiplier + ' (next: cost ' + multiplierCost + ')';
 }
 
+function displayAutoclick() {
+    autoclick.value = 'Autoclick (cost ' + autoclickCost + ')';
+}
 
 function increaseScore() {
     score += clickValue;
     displayScore();
 }
 
+function multiplyEnabler() {
+    if (score >= multplierCost) {
+        multiply.disabled = false;
+    } else {
+        multiply.disabled = true;
+    }
+}
+
 function increaseMultiplier() {
     score -= multiplierCost;
     multiplier += 1;
     clickValue = multiplier;
-    if (bonusOn) {
-        clickValue *= 2;
-    }
+
     multiplierCost *= multiplier;
     displayScore();
     displayMultiplier();
+    
 }
 
 displayScore();
 displayMultiplier();
-multiply.disabled = true;
+autoclick.disabled = true;
 
 click.addEventListener('click', increaseScore);
 multiply.addEventListener('click', increaseMultiplier);
+autoclick.addEventListener('click', enableautoclick);
 
 
