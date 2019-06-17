f9b87a77ee938c30c6e1a852e6f858cd01ecd7b4
Blake <48078206+Blakevdw@users.noreply.github.com>
2019-06-17 11:23:36 +1000

    2nd installment

--- Term 2/Javascript Challenge 2/Challenge_2.js
+++ Term 2/Javascript Challenge 2/Challenge_2.js
@@ -1,7 +1,45 @@
 
+ function myFunction() {
+    alert("You Scored 3/3!");
+}
 
 
 
-function myFunction() {
-    alert("I am an alert box!");
+function check() {
+    
+    var question1 = document.quiz.question1.valuel;
+    var question2 = document.quiz.question2.valuel;
+    var question3 = document.quiz.question3.valuel;
+    var correct = 0;
+
+    if (question1 == "D") {
+        correct++;
+    }
+
+    if (question2 == "D") {
+        correct++;
+    }
+
+    if (question3 == "A") {
+        correct++;
+    }
+
+    if (correct == 0) {
+        score = 2;
+    }
+
+    if (correct > 0 && correct < 3) {
+        score = 1;
+    }
+
+    if (correct == 3) {
+        score = 0;
+    }
+
+    document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("message").innerHTML = messages[score];
+
+    
+   
 }
